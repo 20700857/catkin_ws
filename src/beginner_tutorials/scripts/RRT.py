990fa6e88a52226404d19fbdcb9a4c0bd1e8244a
20700857 <raynhardt.vanzyl@gmail.com>
2020-09-10 09:32:37 +0200

    RRT update

--- src/beginner_tutorials/scripts/RRT.py
+++ src/beginner_tutorials/scripts/RRT.py
@@ -14,6 +14,8 @@ import tf
 from tf import TransformBroadcaster
 import tf2_ros
 import random
+from scipy.stats import norm
+import numpy as np
 class mapNode():
     def __init__(self, position, parent):
         self.location = position
@@ -28,14 +30,16 @@ class fullMap():
         self.RRTPublisher = rospy.Publisher('/RRT', MarkerArray, queue_size=10)
         self.RRTLinesPublisher = rospy.Publisher('/RRTLine', MarkerArray, queue_size=10)
         self.RRTPath = rospy.Publisher('/RRTPath', MarkerArray, queue_size=10) 
+        self.randPoint = rospy.Publisher('/randPoint', MarkerArray, queue_size=10) 
         self.delta = 0.2
         self.pathFound = False
 
     def addNode(self, event = None):
         if not self.pathFound:
             out = Point()
-            out.x = random.random()*10 - 5
-            out.y = random.random()*10 - 5
+            mean = [self.goal.x, self.goal.y]
+            cov = [[2, 0], [0, 2]]  # diagonal covariance
+            out.x , out.y = np.random.multivariate_normal(mean, cov, 1).T
             out.z = 0
             nearest = self.nearestNode(out)
             angle = self.getAngle(nearest.location, out)
@@ -43,8 +47,50 @@ class fullMap():
             nodeAdded.x = nearest.location.x + math.cos(angle)*self.delta
             nodeAdded.y = nearest.location.y + math.sin(angle)*self.delta
             nodeAdded.z = 0
+
+            if True:
+                outPoint = MarkerArray()
+
+                newPoint = Marker()
+                newPoint.id = 1
+                newPoint.header.frame_id = 'map'
+                newPoint.type = newPoint.SPHERE
+                newPoint.action = newPoint.ADD
+                newPoint.scale.x = 0.2
+                newPoint.scale.y = 0.2
+                newPoint.scale.z = 0.2
+                newPoint.pose.position = out
+                newPoint.color.a = 1.0
+                newPoint.color.r = 0.5
+                newPoint.color.g = 0.5
+                newPoint.color.b = 0.5
+                tempOri = tf.transformations.quaternion_from_euler(0.0, 0.0, 0)
+                newPoint.pose.orientation.x = tempOri[0]
+                newPoint.pose.orientation.y = tempOri[1]
+                newPoint.pose.orientation.z = tempOri[2]
+                newPoint.pose.orientation.w = tempOri[3]
+                newPoint.lifetime = rospy.Time(1)
+                outPoint.markers.append(newPoint)
+
+                marker3 = Marker()
+                marker3.id = 1
+                marker3.header.frame_id = 'map'
+                marker3.type = Marker.LINE_STRIP
+                marker3.ns = 'Testline'
+                marker3.action = 0
+                marker3.scale.x = 0.05
+                marker3.points.append(out)
+                marker3.points.append(nearest.location)
+                marker3.color.a = 1.0
+                marker3.color.r = 0.0
+                marker3.color.g = 0.0
+                marker3.color.b = 1.0
+                marker3.lifetime = rospy.Time(1)
+                outPoint.markers.append(marker3)
+                self.randPoint.publish(outPoint)
+
             self.explored.append(mapNode(nodeAdded, nearest))
-            if self.getDistToGoal(nodeAdded) < 0.5:
+            if self.getDistToGoal(nodeAdded) < 0.1:
                 self.pathFound = True
         else:
             temp = MarkerArray()
@@ -66,6 +112,7 @@ class fullMap():
                     marker3.color.r = 1.0
                     marker3.color.g = 1.0
                     marker3.color.b = 0.0
+                    marker3.lifetime = rospy.Time(1)
                     idNum += 1
                     temp.markers.append(marker3)
                     current = current.parent
@@ -93,6 +140,10 @@ class fullMap():
     def getDist(self, point1, point2):
         return math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
     
+    def collisionCheck(self):
+
+        return None
+
     def updateGraphics(self, event = None):
         temp = MarkerArray()
         idNum = 1
@@ -117,6 +168,7 @@ class fullMap():
             tempMarker.pose.orientation.y = tempOri[1]
             tempMarker.pose.orientation.z = tempOri[2]
             tempMarker.pose.orientation.w = tempOri[3]
+            tempMarker.lifetime = rospy.Time(1)
             temp.markers.append(tempMarker)
             idNum += 1
 
@@ -139,6 +191,7 @@ class fullMap():
             tempMarker.pose.orientation.y = tempOri[1]
             tempMarker.pose.orientation.z = tempOri[2]
             tempMarker.pose.orientation.w = tempOri[3]
+            tempMarker.lifetime = rospy.Time(1)
             temp.markers.append(tempMarker)
 
         self.RRTPublisher.publish(temp)
@@ -160,6 +213,7 @@ class fullMap():
                 marker3.color.r = 0.0
                 marker3.color.g = 1.0
                 marker3.color.b = 0.0
+                marker3.lifetime = rospy.Time(1)
                 idNum += 1
                 temp.markers.append(marker3)
         self.RRTLinesPublisher.publish(temp)
@@ -168,8 +222,8 @@ if __name__ == '__main__':
     try:
         rospy.init_node('Map', anonymous=True)
         start = Vector3()
-        start.x = 0
-        start.y = 0
+        start.x = -5
+        start.y = -5
         start.z = 0
         rrtMap = fullMap(start)
         rospy.Timer(rospy.Duration(0.01), rrtMap.addNode)
